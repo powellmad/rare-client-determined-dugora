81e8db0f869d0bd9db3aff87f6a9aceae0ad84c0
emilycentko <emily.centko@gmail.com>
2021-04-27 15:31:03 -0500

    Get all and create category fetch in provider

---
+++ src/components/categories/CategoryProvider.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+
+export const CategoryContext = React.createContext()
+
+export const CategoryProvider = (props) => {
+    const [categories, setCategories] = useState([])
+
+    const getCategories = () => {
+        return fetch("http://localhost:8088/categories")
+            .then(res => res.json())
+            .then(setCategories)
+    }
+
+    const addCategory = category => {
+        return fetch("http://localhost:8088/categories", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(category)
+        })
+            .then(getCategories)
+    }
+
+    return (
+        <CategoryContext.Provider value={{
+            categories, getCategories, addCategory
+        }}>
+            {props.children}
+        </CategoryContext.Provider>
+    )
+}
