e6dcee9c401fce61f66415f7a5eed528da032568
emilycentko <emily.centko@gmail.com>
2021-04-27 15:09:34 -0500

    Added basic files for categories

---
+++ src/components/categories/Category.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Category.css"
+
+export const Category = () => (
+    <section className="category">
+        <h4 className="category__label">{ category.label }</h4>
+    </section>
+)
