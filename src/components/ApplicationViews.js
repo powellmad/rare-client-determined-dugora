73bc3449d35c9ac92f345a85d1994519fe120f84
emilycentko <emily.centko@gmail.com>
2021-04-27 15:45:28 -0500

    Added category list component

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,21 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { CategoryProvider } from "./categories/CategoryProvider"
+import { CategoryList } from "./categories/CategoryList"
 
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            lineHeight: "1.75rem"
-        }}>
-        </main>
-    </>
+    return (
+        <>
+            <main style={{
+                margin: "5rem 2rem",
+                lineHeight: "1.75rem"
+            }}>
+            </main>
+            <CategoryProvider>
+                <Route exact path="/categories">
+                    <CategoryList />
+                </Route>
+            </CategoryProvider>
+        </>
+    )
 }
---
+++ src/components/categories/CategoryList.js
@@ -0,0 +1,28 @@
+import React, { useState, useContext, useEffect } from "react"
+import { CategoryContext } from "./CategoryProvider"
+import Category from "./Category"
+import "./Category.css"
+
+export const CategoryList = () => {
+    const { categories, getCategories } = useContext(CategoryContext)
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+
+    return (
+        <div>
+            <h1>Categories</h1>
+
+            <button onClick={() => history.push("/categories/create")}>
+                New Category
+            </button>
+
+            <div className="categories">
+                {
+                    categories.map(category => <Category key={category.id} category={category}/>)
+                }
+            </div>
+        </div>
+    )
+}
