56a8c1ba7c5a49bed64979f896a67e048f0cb858
powellmad <madsnpowell@gmail.com>
2021-04-29 11:30:10 -0500

    minor change to resolve conflict

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect } from "react"
 import { UserContext } from "./UserProvider"
 import { UserCard } from "./User"
 import "./User.css"
 
-export const UserList = () => {
+export const UserList = (props) => {
     const { users, getUsers } = useContext(UserContext)
 
   useEffect(() => {
@@ -13,13 +13,12 @@ export const UserList = () => {
   return (
     <div className="user-component">
     <h2>Users</h2>
-    
-    <div className="user__list">
-      {
-        users.map(user => {
-          return <UserCard key={user.id} user={user} />
-        })
-      }
+      <div className="user__list">
+        {
+          users.map(user => {
+            return <UserCard key={user.id} user={user} />
+          })
+        }
       </div>
     </div>
   )
