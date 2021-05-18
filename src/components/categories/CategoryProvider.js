f0d1d04072409b766dc1e2594c1287a7f8a10728
dtracydepp <tracydepp82@gmail.com>
2021-05-18 16:06:27 -0500

    updated category providers

--- src/components/categories/CategoryProvider.js
+++ src/components/categories/CategoryProvider.js
@@ -6,48 +6,38 @@ export const CategoryProvider = (props) => {
     const [categories, setCategories] = useState([])
 
     const getCategories = () => {
-        return fetch("http://localhost:8088/categories")
-            .then(res => res.json())
+        return fetch("http://localhost:8000/categories", {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rare_user_token")}`
+            }
+        })
+            .then(response => response.json())
             .then(setCategories)
     }
 
-    const addCategory = category => {
-        return fetch("http://localhost:8088/categories", {
+    const addCategory = (category) => {
+        return fetch('http://localhost:8000/categories', {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Authorization": `Token ${localStorage.getItem("rare_user_token")}`,
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(category)
         })
-            .then(getCategories)
+        .then(getCategories)
     }
+    
 
     const getCategoryById = (id) => {
-        return fetch(`http://localhost:8088/categories/${id}`)
+        return fetch(`http://localhost:8000/categories/${id}`)
             .then(res => res.json())
     }
 
-    const updateCategory = category => {
-        return fetch(`http://localhost:8088/categories/${category.id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(category)
-        })
-          .then(getCategories)
-      }
     
-    const deleteCategory = categoryId => {
-        return fetch(`http://localhost:8088/categories/${categoryId}`, {
-            method: "DELETE"
-        })
-            .then(getCategories)
-    }
 
     return (
         <CategoryContext.Provider value={{
-            categories, getCategories, addCategory, getCategoryById, updateCategory, deleteCategory
+            categories, getCategories, addCategory,getCategoryById
         }}>
             {props.children}
         </CategoryContext.Provider>
