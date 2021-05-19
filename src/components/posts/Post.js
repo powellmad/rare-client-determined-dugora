2152e0ebbb867ed3552d1e905b08d9735ce96e0f
hrlnsnchz <hrlnsnchz@gmail.com>
2021-05-19 15:45:13 -0500

    Renders categories on posts and posts now only render the title and category label respectively.

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,38 +1,19 @@
 import React, {useEffect, useContext } from "react"
 import "./Post.css"
 import { Link } from "react-router-dom"
-import { CategoryContext } from "../categories/CategoryProvider"
 
 export const Post = ({post}) => {
-    const { categories, getCategories } = useContext(CategoryContext)
-    
-    useEffect(() => {
-        getCategories()
-    }, [])
-
-    console.log(post, 'post')
-    
+        
     return (
     <section className="post">
         <h3 className="post__title">
-        <Link to={`/posts/detail/${post.id}`}>
-            { post.title }
-        </Link>
+            <Link to={`/posts/detail/${post.id}`}>
+                { post.title }
+            </Link>
         </h3>
-
-        <div className="post_content">{ post.publication_date }</div>
-        <div className="post_content">{ post.content }</div>
-
-        <div className="post_category">{ 
-        categories.map( category => {
-            if (category.id == post.category_id) {
-                return categories.label
-            }
-        }
-        )} </div>
-        
-        <div className="post_imageUrl">{ post.imageUrl }</div>
+        <div className="post_category">
+            { post.category.label }
+        </div>
     </section>
-
     )
 }
