62fa9d8c13c43fbd4cfd8a805e5201fef2d76890
bpotts11 <brittneympotts@gmail.com>
2021-05-20 15:56:51 -0500

    post details works

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -4,25 +4,26 @@ import "./Post.css"
 import { useParams } from "react-router-dom"
 
 export const PostDetail = (props) => {
-    console.log(props)
-    const { post, getPostById } = useContext(PostContext)
-    const { deletePost } = useContext(PostContext)
-
+    const { getPostById, deletePost } = useContext(PostContext)
+    const [post, setPost] = useState({})
     const { postId } = useParams();
 
     useEffect(() => {
         getPostById(postId)
+            .then((response) => {
+                setPost(response)
+            })
     }, [])
 
     return (
 
         <section className="post">
             <h3 className="post_title">{post.title}</h3>
-            <div className="post_category">{post.category.label}</div>
-            <a className="post_image_url" href={post.image_url} target="_blank">{post.image_url}</a>
+            <div className="post_category">{post.category?.label}</div>
+            <img className="post_image_url" src={post.image_url} />
             <div className="post_publication_date">Published: {post.publication_date}</div>
-            <div className="post_author">By: {post.rareuser.user.first_name}</div>
-            <div className="post_content">Content: {post.content}</div>
+            <div className="post_author">By: {post.user?.user.first_name} {post.user?.user.last_name}</div>
+            <div className="post_content">{post.content}</div>
 
             <button onClick={() => deletePost(post.id).then(() => props.history.push("/posts"))} >Delete Post</button>
 
